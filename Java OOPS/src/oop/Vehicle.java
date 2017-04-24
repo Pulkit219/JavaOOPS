448a053ee13c6203b677ec27b0bea31f8b1281f2
Pulkit219 <ascension1110@gmail.com>
2017-04-23 23:23:45 -0400

    init comm

---
+++ Java OOPS/src/oop/Vehicle.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class Vehicle {
+
+	public String color;
+	public String name;
+	
+	
+	
+public Vehicle(String name, String color){
+		this.color = color;
+		this.name = name;
+		
+	}
+
+}
---
+++ Java OOPS/src/oop/main.java
@@ -0,0 +1,15 @@
+package oop;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Vehicle vehicle1 =new Vehicle("bmw", "white");
+		Vehicle vehicle2 =new Vehicle("Porsche", "white");
+		System.out.println(vehicle1.color);
+		System.out.println(vehicle2.color);
+	}
+
+}
