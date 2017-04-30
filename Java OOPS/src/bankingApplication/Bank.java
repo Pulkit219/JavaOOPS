908ae92457d6caa0e0635dacce43f8c01e4f62f7
Pulkit219 <ascension1110@gmail.com>
2017-04-30 19:57:09 -0400

    2nd comm

--- Java OOPS/src/bankingApplication/Bank.java
+++ Java OOPS/src/bankingApplication/Bank.java
@@ -1,5 +1,24 @@
 package bankingApplication;
 
 public class Bank {
+double 	interestRate =8.5;
+double transactionFees= 10;
+Customer[] customers;
+
+public void calculateInterest(){
+	
+}
+
+public void getInterestRate(){
+	
+}
+
+public void getTransactionFees(){
+	
+}
+
+
+	
+	
 
 }
--- Java OOPS/src/bankingApplication/Customer.java
+++ Java OOPS/src/bankingApplication/Customer.java
@@ -2,4 +2,18 @@ package bankingApplication;
 
 public class Customer {
 
+	String name;
+	Account account;
+	
+	public void getName(){
+		
+	}
+	
+    public void getAccount(){
+		
+	}
+
+    public void display(){
+	
+}
 }
