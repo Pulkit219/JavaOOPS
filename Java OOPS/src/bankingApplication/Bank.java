8c6ae4249c51408dca3051b54f22675ee3282941
Pulkit219 <ascension1110@gmail.com>
2017-05-02 13:35:40 -0400

    8th comm init user input

--- Java OOPS/src/bankingApplication/Bank.java
+++ Java OOPS/src/bankingApplication/Bank.java
@@ -26,6 +26,9 @@ public static int getTransactionFees(){
 	
 }
 
+public Customer[] getCustomer(){
+	 return customers;
+}
 
 	
 	
--- Java OOPS/src/bankingApplication/Main.java
+++ Java OOPS/src/bankingApplication/Main.java
@@ -1,21 +1,39 @@
 package bankingApplication;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
-	
-		Account ac1 = new Account("p123", 100);
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader bufferedreader = new BufferedReader(new InputStreamReader (System.in));
+		while(true){
+			
+			System.out.println("Please enter your choice: ");
+			System.out.println("1: add Customer");
+			System.out.println("2: Deposit Money");
+			System.out.println("3: Withdraw Money");
+			System.out.println("4: Check Balance");
+			System.out.println("5: Calculate interest");
+			System.out.println("6: Exit");
+			int choice = Integer.parseInt(bufferedreader.readLine());
+			
+		}
 		
 		
-		ac1.deposit(2000);
-		
-		
-		
-		ac1.withdraw(100);
-		//now account has exactly 2000 bucks , 2nd transaction
-		ac1.withdraw(1900);
-		System.out.println(ac1.getBalance());
-		System.out.println(ac1.firstTime);
+//		Account ac1 = new Account("p123", 100);
+//		
+//		
+//		ac1.deposit(2000);
+//		
+//		
+//		
+//		ac1.withdraw(100);
+//		//now account has exactly 2000 bucks , 2nd transaction
+//		ac1.withdraw(1900);
+//		System.out.println(ac1.getBalance());
+//		System.out.println(ac1.firstTime);
 		
 
 	}
