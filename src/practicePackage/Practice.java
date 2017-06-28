c5e573466d9e215228a5d3853f5bb353bf2b87ed
Pulkit219 <ascension1110@gmail.com>
2017-06-28 14:05:08 -0400

    2nd comm

--- src/practicePackage/Practice.java
+++ src/practicePackage/Practice.java
@@ -3,6 +3,8 @@ package practicePackage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Practice {
 
@@ -10,29 +12,40 @@ public class Practice {
 		
 
 		
-		int line;
-		String input;
-		 
-		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int line;
+//		String input;
+//		 
+//		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		    
+//		    try {
+//		    	System.out.println("Please enter an ineteger\n");
+//		    	input = br.readLine();
+//		    	 line = Integer.parseInt(input);
+//		    	 for(int i=0; i<line ; i++)
+//				    {
+//				    	System.out.println("hello");
+//				    	
+//				    }
+//			} catch (Exception e) {
+//				 System.out.println("Must enter a number!");
+//		        
+//				
+//			}
 		    
-		    try {
-		    	System.out.println("Please enter an ineteger\n");
-		    	input = br.readLine();
-		    	 line = Integer.parseInt(input);
-		    	 for(int i=0; i<line ; i++)
-				    {
-				    	System.out.println("hello");
-				    	
-				    }
-			} catch (Exception e) {
-				 System.out.println("Must enter a number!");
-		        
-				
+		while(true) {
+			  Scanner input=new Scanner(System.in);
+			  try {
+			     System.out.println("ENTER A NUMBER: ");
+			     int n = input.nextInt();
+			     break;
+			 }catch(InputMismatchException e) {
+			   System.out.println("ERROR!!!");
+			   input.next(); // eat some chars
+			 }
 			}
-		    
 		  
 		    
-//		    System.out.println(input);
+
 		  
 		}
 
